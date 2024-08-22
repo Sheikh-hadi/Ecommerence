5690583642a61cbd5b82adb7f7eb448f03f19b09
Shahzaman099 <mshahzaman358@gmail.com>
2024-08-22 23:58:25 +0500

    image err

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
-import Home from './Pages/Home';
+import "./App.css";
+import Home from "./Pages/Home";
 
 function App() {
-  return (
-    <Home/>
-  )
-
+  return <Home />;
 }
 
 export default App;
